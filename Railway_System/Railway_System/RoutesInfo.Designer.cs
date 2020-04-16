e1edc5996b672012c481341944e6af6f273b979c
aurasnga <auras_gabriel@hotmail.com>
2020-04-17 00:26:26 +0300

    Winform manageriere rute trenuri

---
+++ Railway_System/Railway_System/RoutesInfo.Designer.cs
@@ -0,0 +1,314 @@
+﻿namespace Railway_System
+{
+    partial class RoutesInfo
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.label2 = new System.Windows.Forms.Label();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.label5 = new System.Windows.Forms.Label();
+            this.label6 = new System.Windows.Forms.Label();
+            this.label7 = new System.Windows.Forms.Label();
+            this.departureText = new System.Windows.Forms.TextBox();
+            this.arrivalText = new System.Windows.Forms.TextBox();
+            this.DepartTimeText = new System.Windows.Forms.TextBox();
+            this.ArrivTimeText = new System.Windows.Forms.TextBox();
+            this.ClassText = new System.Windows.Forms.TextBox();
+            this.PriceText = new System.Windows.Forms.TextBox();
+            this.SeatText = new System.Windows.Forms.TextBox();
+            this.button2 = new System.Windows.Forms.Button();
+            this.label8 = new System.Windows.Forms.Label();
+            this.CodeText = new System.Windows.Forms.TextBox();
+            this.groupBox1 = new System.Windows.Forms.GroupBox();
+            this.Routes = new System.Windows.Forms.Label();
+            this.manage = new System.Windows.Forms.Label();
+            this.label9 = new System.Windows.Forms.Label();
+            this.groupBox1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Gadugi", 11.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label2.Location = new System.Drawing.Point(6, 26);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(80, 19);
+            this.label2.TabIndex = 1;
+            this.label2.Text = "Departure";
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Gadugi", 11.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.Location = new System.Drawing.Point(6, 74);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(119, 19);
+            this.label1.TabIndex = 2;
+            this.label1.Text = "Departure Time";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Font = new System.Drawing.Font("Gadugi", 11.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label3.Location = new System.Drawing.Point(6, 50);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(56, 19);
+            this.label3.TabIndex = 3;
+            this.label3.Text = "Arrival";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Font = new System.Drawing.Font("Gadugi", 11.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label4.Location = new System.Drawing.Point(6, 99);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(95, 19);
+            this.label4.TabIndex = 4;
+            this.label4.Text = "Arrival Time";
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Font = new System.Drawing.Font("Gadugi", 11.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label5.Location = new System.Drawing.Point(6, 125);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(44, 19);
+            this.label5.TabIndex = 5;
+            this.label5.Text = "Class";
+            this.label5.Click += new System.EventHandler(this.label5_Click);
+            // 
+            // label6
+            // 
+            this.label6.AutoSize = true;
+            this.label6.Font = new System.Drawing.Font("Gadugi", 11.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label6.Location = new System.Drawing.Point(7, 151);
+            this.label6.Name = "label6";
+            this.label6.Size = new System.Drawing.Size(43, 19);
+            this.label6.TabIndex = 6;
+            this.label6.Text = "Price";
+            // 
+            // label7
+            // 
+            this.label7.AutoSize = true;
+            this.label7.Font = new System.Drawing.Font("Gadugi", 11.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label7.Location = new System.Drawing.Point(6, 175);
+            this.label7.Name = "label7";
+            this.label7.Size = new System.Drawing.Size(46, 19);
+            this.label7.TabIndex = 7;
+            this.label7.Text = "Seats";
+            // 
+            // departureText
+            // 
+            this.departureText.Location = new System.Drawing.Point(124, 25);
+            this.departureText.Name = "departureText";
+            this.departureText.Size = new System.Drawing.Size(139, 20);
+            this.departureText.TabIndex = 8;
+            // 
+            // arrivalText
+            // 
+            this.arrivalText.Location = new System.Drawing.Point(124, 49);
+            this.arrivalText.Name = "arrivalText";
+            this.arrivalText.Size = new System.Drawing.Size(139, 20);
+            this.arrivalText.TabIndex = 9;
+            // 
+            // DepartTimeText
+            // 
+            this.DepartTimeText.AcceptsReturn = true;
+            this.DepartTimeText.Location = new System.Drawing.Point(124, 73);
+            this.DepartTimeText.Name = "DepartTimeText";
+            this.DepartTimeText.Size = new System.Drawing.Size(139, 20);
+            this.DepartTimeText.TabIndex = 10;
+            // 
+            // ArrivTimeText
+            // 
+            this.ArrivTimeText.Location = new System.Drawing.Point(124, 98);
+            this.ArrivTimeText.Name = "ArrivTimeText";
+            this.ArrivTimeText.Size = new System.Drawing.Size(139, 20);
+            this.ArrivTimeText.TabIndex = 11;
+            // 
+            // ClassText
+            // 
+            this.ClassText.Location = new System.Drawing.Point(124, 124);
+            this.ClassText.Name = "ClassText";
+            this.ClassText.Size = new System.Drawing.Size(139, 20);
+            this.ClassText.TabIndex = 12;
+            // 
+            // PriceText
+            // 
+            this.PriceText.Location = new System.Drawing.Point(124, 150);
+            this.PriceText.Name = "PriceText";
+            this.PriceText.Size = new System.Drawing.Size(139, 20);
+            this.PriceText.TabIndex = 13;
+            // 
+            // SeatText
+            // 
+            this.SeatText.Location = new System.Drawing.Point(124, 174);
+            this.SeatText.Name = "SeatText";
+            this.SeatText.Size = new System.Drawing.Size(139, 20);
+            this.SeatText.TabIndex = 14;
+            // 
+            // button2
+            // 
+            this.button2.BackColor = System.Drawing.Color.LightSalmon;
+            this.button2.Cursor = System.Windows.Forms.Cursors.Hand;
+            this.button2.FlatAppearance.BorderSize = 0;
+            this.button2.FlatStyle = System.Windows.Forms.FlatStyle.Popup;
+            this.button2.Font = new System.Drawing.Font("Gadugi", 9.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.button2.ForeColor = System.Drawing.SystemColors.ControlText;
+            this.button2.Location = new System.Drawing.Point(124, 239);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(106, 35);
+            this.button2.TabIndex = 16;
+            this.button2.Text = "Add Train";
+            this.button2.UseVisualStyleBackColor = false;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // label8
+            // 
+            this.label8.AutoSize = true;
+            this.label8.Font = new System.Drawing.Font("Gadugi", 11.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label8.Location = new System.Drawing.Point(6, 203);
+            this.label8.Name = "label8";
+            this.label8.Size = new System.Drawing.Size(84, 19);
+            this.label8.TabIndex = 17;
+            this.label8.Text = "Train Code";
+            // 
+            // CodeText
+            // 
+            this.CodeText.Location = new System.Drawing.Point(124, 202);
+            this.CodeText.Name = "CodeText";
+            this.CodeText.Size = new System.Drawing.Size(139, 20);
+            this.CodeText.TabIndex = 18;
+            // 
+            // groupBox1
+            // 
+            this.groupBox1.BackColor = System.Drawing.Color.OrangeRed;
+            this.groupBox1.Controls.Add(this.CodeText);
+            this.groupBox1.Controls.Add(this.label8);
+            this.groupBox1.Controls.Add(this.button2);
+            this.groupBox1.Controls.Add(this.SeatText);
+            this.groupBox1.Controls.Add(this.PriceText);
+            this.groupBox1.Controls.Add(this.ClassText);
+            this.groupBox1.Controls.Add(this.ArrivTimeText);
+            this.groupBox1.Controls.Add(this.DepartTimeText);
+            this.groupBox1.Controls.Add(this.arrivalText);
+            this.groupBox1.Controls.Add(this.departureText);
+            this.groupBox1.Controls.Add(this.label7);
+            this.groupBox1.Controls.Add(this.label6);
+            this.groupBox1.Controls.Add(this.label5);
+            this.groupBox1.Controls.Add(this.label4);
+            this.groupBox1.Controls.Add(this.label3);
+            this.groupBox1.Controls.Add(this.label1);
+            this.groupBox1.Controls.Add(this.label2);
+            this.groupBox1.Location = new System.Drawing.Point(12, 61);
+            this.groupBox1.Name = "groupBox1";
+            this.groupBox1.Size = new System.Drawing.Size(307, 296);
+            this.groupBox1.TabIndex = 19;
+            this.groupBox1.TabStop = false;
+            // 
+            // Routes
+            // 
+            this.Routes.AutoSize = true;
+            this.Routes.BackColor = System.Drawing.Color.Snow;
+            this.Routes.Font = new System.Drawing.Font("Gadugi", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.Routes.Location = new System.Drawing.Point(169, 20);
+            this.Routes.Name = "Routes";
+            this.Routes.Size = new System.Drawing.Size(61, 19);
+            this.Routes.TabIndex = 20;
+            this.Routes.Text = "Routes";
+            // 
+            // manage
+            // 
+            this.manage.AutoSize = true;
+            this.manage.BackColor = System.Drawing.Color.LightSalmon;
+            this.manage.Font = new System.Drawing.Font("Gadugi", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.manage.ForeColor = System.Drawing.Color.White;
+            this.manage.Location = new System.Drawing.Point(102, 20);
+            this.manage.Name = "manage";
+            this.manage.Size = new System.Drawing.Size(71, 19);
+            this.manage.TabIndex = 21;
+            this.manage.Text = "Manage";
+            // 
+            // label9
+            // 
+            this.label9.AutoSize = true;
+            this.label9.Font = new System.Drawing.Font("Gadugi", 11.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label9.ForeColor = System.Drawing.Color.White;
+            this.label9.Location = new System.Drawing.Point(302, 9);
+            this.label9.Name = "label9";
+            this.label9.Size = new System.Drawing.Size(19, 19);
+            this.label9.TabIndex = 22;
+            this.label9.Text = "X";
+            this.label9.Click += new System.EventHandler(this.label9_Click);
+            // 
+            // RoutesInfo
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.OrangeRed;
+            this.ClientSize = new System.Drawing.Size(333, 380);
+            this.Controls.Add(this.label9);
+            this.Controls.Add(this.manage);
+            this.Controls.Add(this.Routes);
+            this.Controls.Add(this.groupBox1);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.None;
+            this.Name = "RoutesInfo";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "RoutesInfo";
+            this.groupBox1.ResumeLayout(false);
+            this.groupBox1.PerformLayout();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.Label label6;
+        private System.Windows.Forms.Label label7;
+        private System.Windows.Forms.TextBox departureText;
+        private System.Windows.Forms.TextBox arrivalText;
+        private System.Windows.Forms.TextBox DepartTimeText;
+        private System.Windows.Forms.TextBox ArrivTimeText;
+        private System.Windows.Forms.TextBox ClassText;
+        private System.Windows.Forms.TextBox PriceText;
+        private System.Windows.Forms.TextBox SeatText;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.Label label8;
+        private System.Windows.Forms.TextBox CodeText;
+        private System.Windows.Forms.GroupBox groupBox1;
+        private System.Windows.Forms.Label Routes;
+        private System.Windows.Forms.Label manage;
+        private System.Windows.Forms.Label label9;
+    }
+}
---
+++ Railway_System/Railway_System/RoutesInfo.cs
@@ -0,0 +1,60 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Railway_System
+{
+    public partial class RoutesInfo : Form
+    {
+        RailwayDbEntities db;
+        
+        public RoutesInfo()
+        {
+            InitializeComponent();
+            db = new RailwayDbEntities();
+        }
+
+        void Clear()
+        {
+            departureText.Text = arrivalText.Text = DepartTimeText.Text = ArrivTimeText.Text = ClassText.Text = PriceText.Text = SeatText.Text = CodeText.Text = "";
+        }
+
+        private void label1_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void label5_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            Route rd = new Route();
+            rd.TrainCode = CodeText.Text;
+            rd.DepartureCity = departureText.Text;
+            rd.ArrivalCity = arrivalText.Text;
+            rd.DepartureTime = DepartTimeText.Text;
+            rd.ArrivalTime = ArrivTimeText.Text;
+            rd.Class = ClassText.Text;
+            rd.Seat = Convert.ToInt32(SeatText.Text);
+            rd.Price = Convert.ToDecimal(PriceText.Text);
+            db.Routes.Add(rd);
+            db.SaveChanges();
+            MessageBox.Show("New route added successfully !");
+            Clear();
+        }
+
+        private void label9_Click(object sender, EventArgs e)
+        {
+            this.Close();
+        }
+    }
+}
