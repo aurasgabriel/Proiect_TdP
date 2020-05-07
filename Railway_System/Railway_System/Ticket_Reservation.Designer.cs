d2cad737f45be0d93855fecbe54df822af30e555
aurasnga <auras_gabriel@hotmail.com>
2020-05-07 14:21:33 +0300

    Winform pentru rezervare bilete tren

---
+++ Railway_System/Railway_System/Ticket_Reservation.Designer.cs
@@ -0,0 +1,367 @@
+﻿namespace Railway_System
+{
+    partial class Ticket_Reservation
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.label5 = new System.Windows.Forms.Label();
+            this.label6 = new System.Windows.Forms.Label();
+            this.label7 = new System.Windows.Forms.Label();
+            this.label8 = new System.Windows.Forms.Label();
+            this.label9 = new System.Windows.Forms.Label();
+            this.label10 = new System.Windows.Forms.Label();
+            this.label11 = new System.Windows.Forms.Label();
+            this.label13 = new System.Windows.Forms.Label();
+            this.button1 = new System.Windows.Forms.Button();
+            this.DepartureCombo = new System.Windows.Forms.ComboBox();
+            this.ArrivalCombo = new System.Windows.Forms.ComboBox();
+            this.button2 = new System.Windows.Forms.Button();
+            this.dataGrid = new System.Windows.Forms.DataGridView();
+            this.CustomerIDtxt = new System.Windows.Forms.TextBox();
+            this.CustomerFnametxt = new System.Windows.Forms.TextBox();
+            this.CustomerLnametxt = new System.Windows.Forms.TextBox();
+            this.Birthdaytxt = new System.Windows.Forms.TextBox();
+            this.Citytxt = new System.Windows.Forms.TextBox();
+            this.Phonetxt = new System.Windows.Forms.TextBox();
+            this.Mailtxt = new System.Windows.Forms.TextBox();
+            this.Routetxt = new System.Windows.Forms.TextBox();
+            this.Seattxt = new System.Windows.Forms.TextBox();
+            this.button3 = new System.Windows.Forms.Button();
+            this.dateTime = new System.Windows.Forms.DateTimePicker();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGrid)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(12, 9);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(54, 13);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Departure";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(12, 49);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(36, 13);
+            this.label2.TabIndex = 1;
+            this.label2.Text = "Arrival";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(12, 373);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(50, 13);
+            this.label3.TabIndex = 2;
+            this.label3.Text = "Route ID";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(12, 89);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(65, 13);
+            this.label4.TabIndex = 3;
+            this.label4.Text = "Customer ID";
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Location = new System.Drawing.Point(12, 188);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(57, 13);
+            this.label5.TabIndex = 4;
+            this.label5.Text = "First Name";
+            // 
+            // label6
+            // 
+            this.label6.AutoSize = true;
+            this.label6.Location = new System.Drawing.Point(12, 217);
+            this.label6.Name = "label6";
+            this.label6.Size = new System.Drawing.Size(58, 13);
+            this.label6.TabIndex = 5;
+            this.label6.Text = "Last Name";
+            // 
+            // label7
+            // 
+            this.label7.AutoSize = true;
+            this.label7.Location = new System.Drawing.Point(12, 243);
+            this.label7.Name = "label7";
+            this.label7.Size = new System.Drawing.Size(45, 13);
+            this.label7.TabIndex = 6;
+            this.label7.Text = "Birthday";
+            // 
+            // label8
+            // 
+            this.label8.AutoSize = true;
+            this.label8.Location = new System.Drawing.Point(12, 269);
+            this.label8.Name = "label8";
+            this.label8.Size = new System.Drawing.Size(24, 13);
+            this.label8.TabIndex = 7;
+            this.label8.Text = "City";
+            // 
+            // label9
+            // 
+            this.label9.AutoSize = true;
+            this.label9.Location = new System.Drawing.Point(12, 292);
+            this.label9.Name = "label9";
+            this.label9.Size = new System.Drawing.Size(76, 13);
+            this.label9.TabIndex = 8;
+            this.label9.Text = "Phone number";
+            // 
+            // label10
+            // 
+            this.label10.AutoSize = true;
+            this.label10.Location = new System.Drawing.Point(13, 321);
+            this.label10.Name = "label10";
+            this.label10.Size = new System.Drawing.Size(35, 13);
+            this.label10.TabIndex = 9;
+            this.label10.Text = "E-mail";
+            // 
+            // label11
+            // 
+            this.label11.AutoSize = true;
+            this.label11.Location = new System.Drawing.Point(13, 347);
+            this.label11.Name = "label11";
+            this.label11.Size = new System.Drawing.Size(30, 13);
+            this.label11.TabIndex = 10;
+            this.label11.Text = "Date";
+            // 
+            // label13
+            // 
+            this.label13.AutoSize = true;
+            this.label13.Location = new System.Drawing.Point(13, 399);
+            this.label13.Name = "label13";
+            this.label13.Size = new System.Drawing.Size(69, 13);
+            this.label13.TabIndex = 12;
+            this.label13.Text = "Seat Number";
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(82, 503);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(119, 31);
+            this.button1.TabIndex = 13;
+            this.button1.Text = "Make Reservation";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // DepartureCombo
+            // 
+            this.DepartureCombo.FormattingEnabled = true;
+            this.DepartureCombo.Location = new System.Drawing.Point(82, 6);
+            this.DepartureCombo.Name = "DepartureCombo";
+            this.DepartureCombo.Size = new System.Drawing.Size(188, 21);
+            this.DepartureCombo.TabIndex = 14;
+            // 
+            // ArrivalCombo
+            // 
+            this.ArrivalCombo.FormattingEnabled = true;
+            this.ArrivalCombo.Location = new System.Drawing.Point(82, 46);
+            this.ArrivalCombo.Name = "ArrivalCombo";
+            this.ArrivalCombo.Size = new System.Drawing.Size(188, 21);
+            this.ArrivalCombo.TabIndex = 15;
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(311, 27);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(93, 23);
+            this.button2.TabIndex = 16;
+            this.button2.Text = "Search Route";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // dataGrid
+            // 
+            this.dataGrid.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dataGrid.Location = new System.Drawing.Point(291, 99);
+            this.dataGrid.Name = "dataGrid";
+            this.dataGrid.Size = new System.Drawing.Size(387, 435);
+            this.dataGrid.TabIndex = 17;
+            this.dataGrid.CellClick += new System.Windows.Forms.DataGridViewCellEventHandler(this.dataGrid_CellClick);
+            // 
+            // CustomerIDtxt
+            // 
+            this.CustomerIDtxt.Location = new System.Drawing.Point(95, 86);
+            this.CustomerIDtxt.Name = "CustomerIDtxt";
+            this.CustomerIDtxt.Size = new System.Drawing.Size(161, 20);
+            this.CustomerIDtxt.TabIndex = 19;
+            // 
+            // CustomerFnametxt
+            // 
+            this.CustomerFnametxt.Location = new System.Drawing.Point(95, 185);
+            this.CustomerFnametxt.Name = "CustomerFnametxt";
+            this.CustomerFnametxt.Size = new System.Drawing.Size(161, 20);
+            this.CustomerFnametxt.TabIndex = 20;
+            // 
+            // CustomerLnametxt
+            // 
+            this.CustomerLnametxt.Location = new System.Drawing.Point(95, 214);
+            this.CustomerLnametxt.Name = "CustomerLnametxt";
+            this.CustomerLnametxt.Size = new System.Drawing.Size(161, 20);
+            this.CustomerLnametxt.TabIndex = 21;
+            // 
+            // Birthdaytxt
+            // 
+            this.Birthdaytxt.Location = new System.Drawing.Point(95, 240);
+            this.Birthdaytxt.Name = "Birthdaytxt";
+            this.Birthdaytxt.Size = new System.Drawing.Size(161, 20);
+            this.Birthdaytxt.TabIndex = 22;
+            // 
+            // Citytxt
+            // 
+            this.Citytxt.Location = new System.Drawing.Point(95, 266);
+            this.Citytxt.Name = "Citytxt";
+            this.Citytxt.Size = new System.Drawing.Size(161, 20);
+            this.Citytxt.TabIndex = 23;
+            // 
+            // Phonetxt
+            // 
+            this.Phonetxt.Location = new System.Drawing.Point(95, 292);
+            this.Phonetxt.Name = "Phonetxt";
+            this.Phonetxt.Size = new System.Drawing.Size(161, 20);
+            this.Phonetxt.TabIndex = 24;
+            // 
+            // Mailtxt
+            // 
+            this.Mailtxt.Location = new System.Drawing.Point(95, 318);
+            this.Mailtxt.Name = "Mailtxt";
+            this.Mailtxt.Size = new System.Drawing.Size(161, 20);
+            this.Mailtxt.TabIndex = 25;
+            // 
+            // Routetxt
+            // 
+            this.Routetxt.Location = new System.Drawing.Point(95, 370);
+            this.Routetxt.Name = "Routetxt";
+            this.Routetxt.Size = new System.Drawing.Size(161, 20);
+            this.Routetxt.TabIndex = 27;
+            // 
+            // Seattxt
+            // 
+            this.Seattxt.Location = new System.Drawing.Point(95, 396);
+            this.Seattxt.Name = "Seattxt";
+            this.Seattxt.Size = new System.Drawing.Size(161, 20);
+            this.Seattxt.TabIndex = 28;
+            // 
+            // button3
+            // 
+            this.button3.Location = new System.Drawing.Point(124, 130);
+            this.button3.Name = "button3";
+            this.button3.Size = new System.Drawing.Size(104, 25);
+            this.button3.TabIndex = 29;
+            this.button3.Text = "Search Customer";
+            this.button3.UseVisualStyleBackColor = true;
+            this.button3.Click += new System.EventHandler(this.button3_Click);
+            // 
+            // dateTime
+            // 
+            this.dateTime.Location = new System.Drawing.Point(95, 344);
+            this.dateTime.Name = "dateTime";
+            this.dateTime.Size = new System.Drawing.Size(161, 20);
+            this.dateTime.TabIndex = 30;
+            // 
+            // Ticket_Reservation
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(701, 600);
+            this.Controls.Add(this.dateTime);
+            this.Controls.Add(this.button3);
+            this.Controls.Add(this.Seattxt);
+            this.Controls.Add(this.Routetxt);
+            this.Controls.Add(this.Mailtxt);
+            this.Controls.Add(this.Phonetxt);
+            this.Controls.Add(this.Citytxt);
+            this.Controls.Add(this.Birthdaytxt);
+            this.Controls.Add(this.CustomerLnametxt);
+            this.Controls.Add(this.CustomerFnametxt);
+            this.Controls.Add(this.CustomerIDtxt);
+            this.Controls.Add(this.dataGrid);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.ArrivalCombo);
+            this.Controls.Add(this.DepartureCombo);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.label13);
+            this.Controls.Add(this.label11);
+            this.Controls.Add(this.label10);
+            this.Controls.Add(this.label9);
+            this.Controls.Add(this.label8);
+            this.Controls.Add(this.label7);
+            this.Controls.Add(this.label6);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Name = "Ticket_Reservation";
+            this.Text = "Ticket_Reservation";
+            ((System.ComponentModel.ISupportInitialize)(this.dataGrid)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.Label label6;
+        private System.Windows.Forms.Label label7;
+        private System.Windows.Forms.Label label8;
+        private System.Windows.Forms.Label label9;
+        private System.Windows.Forms.Label label10;
+        private System.Windows.Forms.Label label11;
+        private System.Windows.Forms.Label label13;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.ComboBox DepartureCombo;
+        private System.Windows.Forms.ComboBox ArrivalCombo;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.DataGridView dataGrid;
+        private System.Windows.Forms.TextBox CustomerIDtxt;
+        private System.Windows.Forms.TextBox CustomerFnametxt;
+        private System.Windows.Forms.TextBox CustomerLnametxt;
+        private System.Windows.Forms.TextBox Birthdaytxt;
+        private System.Windows.Forms.TextBox Citytxt;
+        private System.Windows.Forms.TextBox Phonetxt;
+        private System.Windows.Forms.TextBox Mailtxt;
+        private System.Windows.Forms.TextBox Routetxt;
+        private System.Windows.Forms.TextBox Seattxt;
+        private System.Windows.Forms.Button button3;
+        private System.Windows.Forms.DateTimePicker dateTime;
+    }
+}
---
+++ Railway_System/Railway_System/Ticket_Reservation.cs
@@ -0,0 +1,115 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Railway_System
+{
+    public partial class Ticket_Reservation : Form
+    {
+        RailwayDbEntities db;
+        public Ticket_Reservation()
+        {
+            InitializeComponent();
+            db = new RailwayDbEntities();
+            BindDeparture();
+            BindArrival();
+
+        }
+
+        private void BindDeparture()
+        {
+            var items = db.Routes.ToList();
+            DepartureCombo.DataSource = items;
+            DepartureCombo.DisplayMember = "DepartureCity";
+            //throw new NotImplementedException();
+        }
+
+        private void BindArrival()
+        {
+            var items = db.Routes.ToList();
+            ArrivalCombo.DataSource = items;
+            ArrivalCombo.DisplayMember = "ArrivalCity";
+            //throw new NotImplementedException();
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            if (ArrivalCombo.Text!= DepartureCombo.Text)
+            {
+                dataGrid.DataSource = db.Routes.Where(a => a.DepartureCity.Equals(DepartureCombo.Text) && a.ArrivalCity.Equals(ArrivalCombo.Text)).ToList();
+            }
+        }
+
+        private void button3_Click(object sender, EventArgs e)
+        {
+            int id = Convert.ToInt32(CustomerIDtxt.Text);
+            var item = db.Customers.Where(a => a.CustomerID == id).FirstOrDefault();
+            CustomerFnametxt.Text = item.First_Name;
+            CustomerLnametxt.Text = item.Last_Name;
+            Birthdaytxt.Text = item.Birthday;
+            Citytxt.Text = item.City;
+            Phonetxt.Text = item.Phone;
+            Mailtxt.Text = item.E_mail;
+        }
+
+        private void dataGrid_CellClick(object sender, DataGridViewCellEventArgs e)
+        {
+            var trainid = dataGrid.SelectedRows[0].Cells[0].Value;
+            Routetxt.Text = trainid.ToString();  
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            if(Convert.ToInt32(Seattxt.Text)<= 200)
+            {
+                if (AvailableSeat() == true)
+                {
+                    Reservation reservation = new Reservation();
+                    reservation.CustomerID = Convert.ToInt32(CustomerIDtxt.Text);
+                    reservation.DateOfDeparture = dateTime.Value;
+                    reservation.RouteID = Convert.ToInt32(Routetxt.Text);
+                    reservation.SeatNr = Convert.ToInt32(Seattxt.Text);
+                    db.Reservations.Add(reservation);
+                    db.SaveChanges();
+                    MessageBox.Show("Ticket Booked Successfully");
+                }
+                else
+                {
+                    MessageBox.Show("Seat is already reserved");
+                }
+            }
+            else
+            {
+                MessageBox.Show("Seat number invalid");
+            }
+        }
+
+        private bool AvailableSeat()
+        {
+            int routeid = Convert.ToInt32(Routetxt.Text);
+            int seatnr = Convert.ToInt32(Seattxt.Text);
+            string departure = dateTime.Value.ToString("dd/MM/YYYY");
+            var item = db.Reservations.Where(a => a.RouteID == routeid && a.SeatNr == seatnr).FirstOrDefault();
+            if (item != null)
+            {
+                string existdate = ((DateTime)item.DateOfDeparture).ToString("dd/MM/YYYY");
+
+                if (existdate == departure)
+
+                    return false;
+                else
+                    return true;
+            }
+            else
+            {
+                return true;
+            }
+        }
+    }
+}
